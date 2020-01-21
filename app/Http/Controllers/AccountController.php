11ed8d71373f0880c478a013c277866d2289a9ac
loveisbluenova <alekssoftdev@gmail.com>
2020-01-21 12:00:29 -0500

    update view profile page

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Map;
+use App\Organization;
+use Sentinel;
 
 
 class AccountController extends Controller
@@ -12,6 +14,9 @@ class AccountController extends Controller
     public function account()
     {
         $map = Map::find(1);
-        return view('frontEnd.account', compact('map'));
+        $user = Sentinel::getUser();
+        $user_organizationid_list = explode(',', $user->user_organization);
+        $organization_list = Organization::whereIn('organization_recordid', $user_organizationid_list)->select('organization_recordid', 'organization_name')->orderby('organization_recordid')->get();
+        return view('frontEnd.account', compact('map', 'user', 'organization_list'));
     }
 }
--- resources/views/frontEnd/account.blade.php
+++ resources/views/frontEnd/account.blade.php
@@ -16,10 +16,10 @@
     			<div class="card">
     				<div class="card-block">
     					<div class="card-title">
-    						@if (Sentinel::getUser())
+    						@if ($user)
     						<h2>
     							<a href="">
-									{{Sentinel::getUser()->first_name}} {{Sentinel::getUser()->last_name}}
+									{{$user->first_name}} {{$user->last_name}}
 	                            </a>
 	                            <a href="" class="btn btn-floating btn-success waves-effect waves-classic" style="float: right;">
 	                                <i class="icon md-edit" style="margin-right: 0px;"></i>
@@ -28,20 +28,26 @@
                             
                             <h4>
 								<span class="badge bg-red pl-0 organize_font"><b>First Name:</b></span> 
-								{{Sentinel::getUser()->first_name}}
+								{{$user->first_name}}
 		                    </h4>
 		                    <h4>
 								<span class="badge bg-red pl-0 organize_font"><b>Last Name:</b></span> 
-								{{Sentinel::getUser()->last_name}}
+								{{$user->last_name}}
 		                    </h4>
 		                    <h4>
 								<span class="badge bg-red pl-0 organize_font"><b>Email:</b></span> 
-								{{Sentinel::getUser()->email}}
+								{{$user->email}}
 		                    </h4>
-		                    <h4>
-								<span class="badge bg-red pl-0 organize_font"><b>Organization:</b></span> 
-								{{Sentinel::getUser()->user_organization}}
-		                    </h4>
+			                    @if ($organization_list)
+			                    <h4>
+									<span class="badge bg-red pl-0 organize_font"><b>Organization:</b></span> 
+									<br>
+									@foreach($organization_list as $organization)
+      									&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; {{$organization->organization_name}} 
+      									<br>
+      								@endforeach
+			                    </h4>
+			                    @endif
 		                    @endif
                         </div>
     				</div>
