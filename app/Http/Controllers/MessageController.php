556e4242764c7e371375e6a13e8b91bec2b094db
loveisbluenova <alekssoftdev@gmail.com>
2020-02-17 08:07:19 -0500

    save messagecredentials into env file in campaigns page of backend

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -80,6 +80,61 @@ class MessageController extends Controller
         return view('backEnd.messages.messageSetting', compact('twillioSid', 'twillioKey', 'twllioNumber', 'sendgridKey'));
     }
 
+
+    public function saveMessageCredential(Request $request)
+    {
+        $this->validate($request, [
+            'twillioSid' => 'required',
+            'twillioKey' => 'required',
+            'twillioNumber' => 'required',
+            'sendgridApiKey' => 'required',
+        ]);
+
+        try {
+
+            $envFile = app()->environmentFilePath();
+            $str = file_get_contents($envFile);
+            $values = [
+                "TWILIO_SID" => $request->get('twillioSid'),
+                "TWILIO_TOKEN" => $request->get('twillioKey'),
+                "TWILIO_FROM" => $request->get('twillioNumber'),
+                "SENDGRID_API_KEY" => $request->get('sendgridApiKey'),
+                "MAIL_PASSWORD" => $request->get('sendgridApiKey'),
+            ];
+
+            if (count($values) > 0) {
+                foreach ($values as $envKey => $envValue) {
+
+                    $str .= "\n"; // In case the searched variable is in the last line without \n
+                    $keyPosition = strpos($str, "{$envKey}=");
+                    $endOfLinePosition = strpos($str, "\n", $keyPosition);
+                    $oldLine = substr($str, $keyPosition, $endOfLinePosition - $keyPosition);
+
+                    // If key does not exist, add it
+                    if (!$keyPosition || !$endOfLinePosition || !$oldLine) {
+                        $str .= "{$envKey}={$envValue}\n";
+                    } else {
+                        $str = str_replace($oldLine, "{$envKey}={$envValue}", $str);
+                    }
+
+                }
+            }
+
+            $str = substr($str, 0, -1);
+            if (!file_put_contents($envFile, $str)) {
+                return false;
+            }
+            $this->clearCache();
+
+            return redirect()->back()->with('success', 'Credential store successfully!');
+
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', 'Something went wrong in input!');
+
+        }
+
+    }
+
     /**
      * Remove the specified resource from storage.
      *
